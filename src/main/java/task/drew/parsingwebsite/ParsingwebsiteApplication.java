fe2a46dccafdd24b70e4b58b98d72a6d087889c2
Drew <goodchickenboy@mail.ru>
2019-09-11 20:53:58 +0400

    init

---
+++ src/main/java/task/drew/parsingwebsite/ParsingwebsiteApplication.java
@@ -0,0 +1,13 @@
+package task.drew.parsingwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParsingwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParsingwebsiteApplication.class, args);
+	}
+
+}
